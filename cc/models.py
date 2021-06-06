1848b048363763aad126a6e5e67b4e4e82e6a50e
medha0105 <you@example.com>
2021-06-06 20:44:47 +0530

    Init commit

---
+++ cc/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Customer(models.Model):
+    CATEGORY = (
+        ('Active','Active'),
+        ('Moderately active','Moderately active'),
+        ('Inactive','Inactive')
+    )
+    GENDER = (
+        ('Male','Male'),
+        ('Female','Female')
+    )
+    name = models.CharField(max_length=200,null=True)
+    email = models.CharField(max_length=100,unique=True,null=True)
+    date_created = models.DateTimeField(auto_now_add)
+    phone = models.CharField(max_length=10, null=True)
+    weight = models.DecimalField()
+    height = models.DecimalField()
+    gender = models.CharField(max_length=50, choices=GENDER)
+    category = models.CharField(max_length=50, choices=CATEGORY)
+    food = models.ForeignKey(Food, on_delete=models.CASCADE)
