9a7cc0ffcb6c5a9cb1517022413535580a7f204c
Krutika Patil <krutikapatil35@gmail.com>
2021-06-06 21:29:21 +0530

    Date_created field changed

--- cc/models.py
+++ cc/models.py
@@ -12,7 +12,7 @@ class Customer(models.Model):
     )
     name = models.CharField(max_length=200,null=True)
     email = models.CharField(max_length=100,unique=True,null=True)
-    date_created = models.DateTimeField(auto_now_add)
+    date_created = models.DateTimeField(auto_now_add=True)
     phone = models.CharField(max_length=10, null=True)
     weight = models.DecimalField()
     height = models.DecimalField()
