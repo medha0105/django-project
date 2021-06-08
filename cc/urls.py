d982dbf82a50394f0e47e2b8ffedff3765a51a7f
medha0105 <you@example.com>
2021-06-08 18:32:07 +0530

    ModelForm added

--- cc/urls.py
+++ cc/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name="home"),
-
+    path('customer_details/',views.customerDetails,name="customer_details")
 ]
--- cc/views.py
+++ cc/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
+from .forms import CustomerForm
 
 
 def home(request):
     return render(request,'cc/home.html')
     
+def customerDetails(request):
+    form = CustomerForm()
+    if request.method == "POST":
+        form = CustomerForm(request.POST)   #request.POST has all data submitted to customer_details url
+        if form.is_valid():                 #is_valid is a method/function to check if form values are valid
+            form.save()
+            return redirect('/')
+
+    context = {'form':form}
+    return render(request,'cc/custom_details.html',context)
 
 
